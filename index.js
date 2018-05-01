449602d622b0ba225e93b5201ea9cf4e494bbc81
LeChat-Modding <LeChatModding@gmail.com>
2018-05-01 16:30:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,19 @@ client.on("ready", () => {
 });
 
 client.on('message', async message => {
+    
+    
     if(message.content.startsWith("/") || message.content.startsWith(":")) {
-       message.guild.channels.find('name', "logs").send(`${message.author} a envoyer la commande ${message.author.lastMessage}`).catch(console.error);
+        let chanelLog = message.guild.channels.find("name", "logs");
+        if(!chanelLog){
+            try{
+                message.guild.createChannel('logs', "text");
+            } catch (e) {
+                console.log(e.stack)
+            }
+        }
+       await(message.guild.channels.find('name', "logs").send(`${message.author} a envoyer la commande ${message.author.lastMessage}`).catch(console.error));
     }
-
+    
+    
 });
